fd6a960781d8e4e49abead88582dbc470c327559
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-09-10 11:55:57 +0300

    Voice Recording step is done

--- src/VoiceRecorderIII.jsx
+++ src/VoiceRecorderIII.jsx
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import { createElement, useState, useEffect } from "react";
 import { Button, View, Text, PermissionsAndroid } from "react-native";
 import Permissions from "react-native-permissions";
@@ -10,25 +11,28 @@ export function VoiceRecorderIII({ yourName, style }) {
     const [audioFile, setAudioFile] = useState("");
     const [loaded, setLoaded] = useState(false);
     const [paused, setPaused] = useState(true);
-
+    const [lastSound, setLastSound] = useState(null);
     useEffect(() => {
-        async function checkPermissionState() {
-            await checkPermission();
-        }
-        checkPermissionState();
-        const options = {
-            sampleRate: 16000, // default 44100
-            channels: 1, // 1 or 2, default 1
-            bitsPerSample: 16, // 8 or 16, default 16// android only (see below)
-            wavFile: "test.wav" // default 'audio.wav'
-        };
-        AudioRecord.init(options);
-        AudioRecord.on("data", data => {
-            // base64-encoded audio data chunks
-            console.info("data", data);
-            const chunk = Buffer.from(data, "base64");
-            console.info("chunk size", chunk.byteLength);
-        });
+        checkPermission()
+            .then(() => {
+                const options = {
+                    sampleRate: 16000, // default 44100
+                    channels: 1, // 1 or 2, default 1
+                    bitsPerSample: 16, // 8 or 16, default 16// android only (see below)
+                    wavFile: "test.wav" // default 'audio.wav'
+                };
+                AudioRecord.init(options);
+                console.info("AudioRecord", AudioRecord);
+                AudioRecord.on("data", data => {
+                    // base64-encoded audio data chunks
+                    console.info("data", data);
+                    const chunk = Buffer.from(data, "base64");
+                    console.info("chunk size", chunk.byteLength);
+                });
+            })
+            .catch(e => {
+                console.error("permission denied", e);
+            });
     }, []);
 
     const requestPremission = () => {
@@ -57,7 +61,7 @@ export function VoiceRecorderIII({ yourName, style }) {
     };
 
     const start = () => {
-        console.info("start recording");
+        console.info("start recording", AudioRecord);
         setAudioFile("");
         setRecording(true);
         setLoaded(false);
@@ -76,11 +80,13 @@ export function VoiceRecorderIII({ yourName, style }) {
             if (!audioFile) {
                 return reject("file path is empty");
             }
+
             sound = new Sound(audioFile, "", error => {
                 if (error) {
                     console.info("failed to load the sound", error);
                     return reject(error);
                 }
+                setLastSound(sound);
                 setLoaded(true);
                 return resolve();
             });
@@ -97,17 +103,26 @@ export function VoiceRecorderIII({ yourName, style }) {
         }
         setPaused(false);
         Sound.setCategory("Playback");
+        console.info("start playing", sound);
+        if (sound === null) {
+            sound = lastSound;
+        }
         sound.play(success => {
             if (success) {
                 console.info("successfully finished playing");
+                setPaused(true);
             } else {
                 console.info("playback failed due to audio decoding errors");
             }
         });
-        setPaused(true);
+
+        setPaused(false);
     };
 
     const pause = () => {
+        if (!sound) {
+            sound = lastSound;
+        }
         sound.pause();
         setPaused(true);
     };
